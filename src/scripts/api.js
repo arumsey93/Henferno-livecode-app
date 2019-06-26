991884b6f7f9f56ff9896d97996ed94b262af235
Alex Rumsey <alexrumsey1993@gmail.com>
2019-06-26 10:48:09 -0500

    refactor menu item fetch for better separation of concerns

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,12 +1,10 @@
-import { makeMenuItemComponent, listMenuItems } from "./domstuff.js"
 
-fetch("http://localhost:8088/menu_items")
+function getMenuItems() {
+return fetch("http://localhost:8088/menu_items")
 .then(menuData => menuData.json())
-.then(menuItems => {
-    // loop thru menu items, and turn them into html components
-    let componentArray = menuItems.map( (oneMenuItemObj) =>{
-        return makeMenuItemComponent(oneMenuItemObj)
-    })
-    // add the menu items components to the DOM
-    listMenuItems(componentArray)
-})
+}
+
+
+
+
+export { getMenuItems }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
-import "./api.js"
+import {getMenuItems} from "./api.js"
+import {makeMenuItemComponent, listMenuItems} from "./domstuff.js"
+
+getMenuItems()
+.then (menuItems => {
+    // loop thru menu items, and turn them into html components
+    let componentArray = menuItems.map( (oneMenuItemObj) =>{
+        return makeMenuItemComponent(oneMenuItemObj)
+    })
+    // add the menu items components to the DOM
+    listMenuItems(componentArray)
+})
